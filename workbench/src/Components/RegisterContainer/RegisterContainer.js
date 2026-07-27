3e1b9532f4dfaff2b35d935ddd5f5fe3ddaeaae8
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-07-27 08:46:31 -0400

    Update synthesizer version.

--- workbench/src/Components/RegisterContainer/RegisterContainer.js
+++ workbench/src/Components/RegisterContainer/RegisterContainer.js
@@ -19,20 +19,27 @@ export function RegisterContainer ({}) {
         editor.onDidChangeModelContent((e) => {});
     }, []);
 
+    const handleEditorMount2 = useCallback((editor, monaco) => {
+        editorRef.current = editor;
+        editor.onDidChangeModelContent((e) => {});
+    }, []);
+
     return (
-        <div style={{width: "100%", height: "100%"}}>
-            <Editor
-                defaultLanguage={"plaintext"}
-                defaultValue=""
-                theme="vs-dark"
-                onMount={handleEditorMount}
-                options={{
-                    minimap: {enabled: false},
-                    lineNumbers: "off",
-                    wordWrap: "on",
-                    scrollBeyondLastLine: false
-                }}
-            />
+        <div style={{height: "100%", display: "flex", flexDirection: "column"}}>
+            <div style={{width: "100%", display: "flex", flexGrow: "1"}}>
+                <Editor
+                    defaultLanguage={"python"}
+                    defaultValue=""
+                    theme="vs-dark" 
+                    onMount={handleEditorMount2}
+                    options={{
+                        minimap: {enabled: false},
+                        lineNumbers: "off",
+                        wordWrap: "on",
+                        scrollBeyondLastLine: false
+                    }}
+                />
+            </div>
         </div>
     );
 }
