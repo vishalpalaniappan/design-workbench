1c339f3c62371947a9b25ea12b473f0dcd7b3b13
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-07-27 19:04:21 -0400

    Save ast to temp file for inspection.

--- server/src/design-file-utils/synthesizeDesign.js
+++ server/src/design-file-utils/synthesizeDesign.js
@@ -6,6 +6,10 @@ import fs from 'fs/promises';
 import synthesisRunner from '../runners/synthesisRunner.js';
 
 async function synthesizeDesign(designName, ast) {
+    const tempDir = path.join(process.cwd(), "temp");
+    const traceFilePath = path.join(tempDir, "ast.json");
+    await fs.writeFile(traceFilePath, JSON.stringify(ast, null, 4));
+
     let synthesizedOutput;
     try {
         synthesizedOutput = await synthesisRunner(JSON.stringify(ast));
